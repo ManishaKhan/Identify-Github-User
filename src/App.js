b151cc8170b68fc0d9d91e0b1909aee21e340184
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-06-25 13:41:49 +0530

    identify-github-user

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import GithubUsers from './GithusUsers';
+
+
+function App() {
+  return (
+    <div className="App">
+    <GithubUsers/>
+    </div>
+  );
+}
+
+export default App;
